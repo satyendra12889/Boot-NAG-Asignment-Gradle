628f335b6d6d9370262e3ab7c1d53b4e6e2d6c3e
satyendra128 <satyendra@nagarro.com>
2020-06-07 13:25:46 +0530

    first setup with gradle

---
+++ src/main/java/com/satyendra/nagarro/demo/application/BootNAGApplicationGradle/BootNagApplicationGradleApplication.java
@@ -0,0 +1,13 @@
+package com.satyendra.nagarro.demo.application.BootNAGApplicationGradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootNagApplicationGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootNagApplicationGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/satyendra/nagarro/demo/application/BootNAGApplicationGradle/BootNagApplicationGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satyendra.nagarro.demo.application.BootNAGApplicationGradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootNagApplicationGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
